0edb326575cb2a2cb05c7a727bd2df255f9ee400
Chris Rees <crees@covermymeds.com>
2016-08-24 20:26:13 -0400

    Add better bounds checking.
    There's an issue where sometimes hitting a boundary will completely stop
    the box

--- lib/pong.ex
+++ lib/pong.ex
@@ -1,8 +1,12 @@
 defmodule Pong do
+  @height 50
+  @width 50
+  @limit 500
+
   def start do
     :ok = :sdl.start([:video])
     :ok = :sdl.stop_on_exit
-    {:ok, window} = :sdl_window.create('Hello SDL', 10, 10, 500, 500, [])
+    {:ok, window} = :sdl_window.create('Hello SDL', 10, 10, @limit, @limit, [])
     {:ok, renderer} = :sdl_renderer.create(window, -1, [:accelerated, :present_vsync])
     loop(renderer, 100, 100, rand_vel, rand_vel)
   end
@@ -11,25 +15,46 @@ defmodule Pong do
     :ok = :sdl_renderer.set_draw_color(renderer, 0, 0, 0, 255)
     :ok = :sdl_renderer.clear(renderer)
     :ok = :sdl_renderer.set_draw_color(renderer, 255, 255, 255, 255)
-    :ok = :sdl_renderer.fill_rect(renderer, %{x: x, y: y, w: 50, h: 50})
+    :ok = :sdl_renderer.fill_rect(renderer, %{x: x, y: y, w: @width, h: @height})
     :ok = :sdl_renderer.present(renderer)
+
     case :sdl_events.poll do
       %{type: :quit} -> :erlang.terminate
-      _ -> new_x = x + x_vel
-           new_y = y + y_vel
-           loop(renderer, new_x, new_y, reverse_vel(new_x + 50, 500, x_vel), reverse_vel(new_y + 50, 500, y_vel))
+      _ -> new_x = new_coord(x, x_vel, @width)
+           new_y = new_coord(y, y_vel, @height)
+           loop(renderer, new_x, new_y, reverse_vel(new_x, x_vel, @width), reverse_vel(new_y, y_vel, @height))
     end
   end
 
-  def reverse_vel(val, limit, vel) do
-    if val <= 0 || val >= limit do
-      if vel < 0 do
-        rand_vel
-      else
-        rand_vel * -1
-      end
-    else
-      vel
+  def new_coord(val, vel, size) do
+    min_val = val + vel
+    max_val = val + vel + size
+    cond do
+      min_val < 0 ->
+        0
+      max_val >= @limit ->
+        @limit - size
+      true ->
+        val + vel
+    end
+  end
+
+  def reverse_vel(val, vel, size) do
+    cond do
+      val <= 0 ->
+        if vel < 0 do
+          rand_vel
+        else
+          vel
+        end
+      val + size >= @limit ->
+        if vel > 0 do
+          rand_vel * -1
+        else
+          vel
+        end
+      true ->
+        vel
     end
   end
 
