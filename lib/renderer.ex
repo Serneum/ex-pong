a20183ef88f2a7028a52c3ec41f1dcd0698eac3d
Chris Rees <crees@covermymeds.com>
2016-08-29 19:55:56 -0400

    Add rendering for a circle. TODO: Put in a PR to esdl2

--- lib/renderer.ex
+++ lib/renderer.ex
@@ -16,7 +16,7 @@ defmodule Renderer do
   end
 
   def draw(renderer, %Ball{} = ball) do
-    draw_rect(renderer, ball.x, ball.y, Ball.width, Ball.height)
+    draw_circle(renderer, ball.x, ball.y, round(Ball.width / 2))
   end
 
   def draw(renderer, paddle) do
@@ -27,6 +27,44 @@ defmodule Renderer do
   defp draw_rect(renderer, x, y, width, height) do
     :ok = set_color(renderer, 255, 255, 255)
     :ok = :sdl_renderer.fill_rect(renderer, %{x: x, y: y, w: width, h: height})
+  end
+
+  defp draw_circle(renderer, x0, y0, radius) do
+    points = midpoint_circle([], x0, radius, y0, 0, 0)
+    :ok = set_color(renderer, 255, 255, 255)
+    :ok = :sdl_renderer.draw_points(renderer, points)
+  end
+
+  defp midpoint_circle(points, x0, x, y0, y, err) when x >= y do
+    points = points
+    |> List.insert_at(-1, %{x: x0 + x, y: y0 + y})
+    |> List.insert_at(-1, %{x: x0 + y, y: y0 + x})
+    |> List.insert_at(-1, %{x: x0 - y, y: y0 + x})
+    |> List.insert_at(-1, %{x: x0 - x, y: y0 + y})
+    |> List.insert_at(-1, %{x: x0 - x, y: y0 - y})
+    |> List.insert_at(-1, %{x: x0 - y, y: y0 - x})
+    |> List.insert_at(-1, %{x: x0 + y, y: y0 - x})
+    |> List.insert_at(-1, %{x: x0 + x, y: y0 - y})
+
+    new_y = y + 1
+    tmp_err = err + 1 + (2 * new_y)
+    values = midpoint_circle_update_values(x, tmp_err)
+
+    midpoint_circle(points, x0, values.x, y0, new_y, values.err)
+  end
+
+  defp midpoint_circle(points, _, _, _, _, _) do
+    points
+  end
+
+  defp midpoint_circle_update_values(x, err) when 2 * (err - x) + 1 > 0 do
+    new_x = x - 1
+    new_err = err + 1 - (2 * new_x)
+    %{x: new_x, err: new_err}
+  end
+
+  defp midpoint_circle_update_values(x, err) do
+    %{x: x, err: err}
   end
 
   defp set_color(renderer, r, g, b) do
