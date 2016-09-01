17fe676fd27a77dc6b541221ae36abb5182dc95a
Chris Rees <crees@covermymeds.com>
2016-09-01 02:24:10 -0400

    Code cleanup

--- lib/renderer.ex
+++ lib/renderer.ex
@@ -38,45 +38,55 @@ defmodule Renderer do
     # :ok = :sdl_renderer.draw_points(renderer, points)
 
     # Draw lines between points from the midpoint-cirlce algorithm to fill the circle
-    # :ok = draw_lines(renderer, x0 + radius, radius, y0 + radius, 0, 0)
+    # ok = draw_lines(renderer, x0 + radius, radius, y0 + radius, 0, 0)
 
     # Draw all points contained within a circle
-    points = draw_points_outer(x0, x0 + radius, y0, y0 + radius, radius)
+    points = get_fill_points_outer(x0, x0 + radius, y0, y0 + radius, radius)
     :ok = :sdl_renderer.draw_points(renderer, points)
+
+    # Draw a series of smaller circles to fill a larger circle
+    # draw_circles(renderer, x0, y0, radius, 1)
   end
 
-  defp draw_points_outer(x, xCenter, y, yCenter, radius) when x <= xCenter do
-    draw_points_inner(x, xCenter, y, yCenter, radius) ++ draw_points_outer(x + 1, xCenter, y, yCenter, radius)
-    |> List.flatten
+  # Draw a series of smaller circles to fill a larger circle
+  defp draw_circles(renderer, x, y, radius, curr_radius) when curr_radius <= radius do
+    points = midpoint_circle(x + radius, curr_radius, y + radius, 0, 0)
+    :ok = :sdl_renderer.draw_points(renderer, points)
+    draw_circles(renderer, x, y, radius, curr_radius + 1)
   end
 
-  defp draw_points_outer(_, _, _, _, _) do
+  defp draw_circles(_, _, _, _, _) do
+    :ok
+  end
+
+  # Draw all points contained within a circle
+  defp get_fill_points_outer(x, xCenter, y, yCenter, radius) when x <= xCenter do
+    get_fill_points_inner(x, xCenter, y, yCenter, radius) ++ get_fill_points_outer(x + 1, xCenter, y, yCenter, radius)
+  end
+
+  defp get_fill_points_outer(_, _, _, _, _) do
     []
   end
 
-  defp draw_points_inner(x, xCenter, y, yCenter, radius) when y <= yCenter do
-    get_points(x, xCenter, y, yCenter, radius) ++ draw_points_inner(x, xCenter, y + 1, yCenter, radius)
+ defp get_fill_points_inner(x, xCenter, y, yCenter, radius) when y <= yCenter do
+    get_fill_points(x, xCenter, y, yCenter, radius) ++ get_fill_points_inner(x, xCenter, y + 1, yCenter, radius)
   end
 
-  defp draw_points_inner(_, _, _, _, _) do
+  defp get_fill_points_inner(_, _, _, _, _) do
     []
   end
 
-  defp get_points(x, xCenter, y, yCenter, radius) when ((x - xCenter) * (x - xCenter) + (y - yCenter) * (y - yCenter)) <= radius * radius do
+ defp get_fill_points(x, xCenter, y, yCenter, radius) when (x - xCenter) * (x - xCenter) + (y - yCenter) * (y - yCenter) < radius * radius do
     xSym = xCenter - (x - xCenter)
     ySym = yCenter - (y - yCenter)
-
-    points = []
-    |> List.insert_at(-1, %{x: x, y: y})
-    |> List.insert_at(-1, %{x: x, y: ySym})
-    |> List.insert_at(-1, %{x: xSym, y: y})
-    |> List.insert_at(-1, %{x: xSym, y: ySym})
+    [%{x: x, y: y}, %{x: x, y: ySym}, %{x: xSym, y: y}, %{x: xSym, y: ySym}]
   end
 
-  defp get_points( _, _, _, _, _) do
+  defp get_fill_points(_, _, _, _, _) do
     []
   end
 
+  # Draw lines between points from the midpoint-cirlce algorithm to fill the circle
   defp draw_lines(renderer, x0, x, y0, y, err) when x >= y do
     :sdl_renderer.draw_line(renderer, %{x: x0 + x, y: y0 + y}, %{x: x0 - x, y: y0 - y})
     :sdl_renderer.draw_line(renderer, %{x: x0 + y, y: y0 + x}, %{x: x0 - y, y: y0 - x})
@@ -94,16 +104,10 @@ defmodule Renderer do
     :ok
   end
 
+  # Use midpoint-circle algorithm to get the points for the outline of a circle
   defp midpoint_circle(x0, x, y0, y, err) when x >= y do
-    points = []
-    |> List.insert_at(-1, %{x: x0 + x, y: y0 + y})
-    |> List.insert_at(-1, %{x: x0 + y, y: y0 + x})
-    |> List.insert_at(-1, %{x: x0 - y, y: y0 + x})
-    |> List.insert_at(-1, %{x: x0 - x, y: y0 + y})
-    |> List.insert_at(-1, %{x: x0 - x, y: y0 - y})
-    |> List.insert_at(-1, %{x: x0 - y, y: y0 - x})
-    |> List.insert_at(-1, %{x: x0 + y, y: y0 - x})
-    |> List.insert_at(-1, %{x: x0 + x, y: y0 - y})
+    points = [%{x: x0 + x, y: y0 + y}, %{x: x0 + y, y: y0 + x}, %{x: x0 - y, y: y0 + x}, %{x: x0 - x, y: y0 + y},
+              %{x: x0 - x, y: y0 - y}, %{x: x0 - y, y: y0 - x}, %{x: x0 + y, y: y0 - x}, %{x: x0 + x, y: y0 - y}]
     |> Enum.uniq
 
     new_y = y + 1
